d7ac4509b3a21e7a35c88465f5e4e8e7672c3c9d
Felipe Feitosa <pro.felipefeitosa@gmail.com>
2021-11-20 10:36:01 -0300

    class has been created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
 
 (function(){
 
-    
+    class Form{
+        constructor(){
+            this.form = document.querySelector('form');
+            this.trackEvent = this.submit();
+        }
+
+        submit(){
+            this.form.addEventListener('submit', e => {
+                e.preventDefault();
+              //se senha os campos forem válidos, submeter form
+              const validField;
+              const validPassword;
+            })
+        }
+
+        
+
+
+    }
 
 })();
